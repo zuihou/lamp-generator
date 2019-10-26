12b11c6bb11de29fd456934be81a9a020c5fbe06
tangyh <244387066@qq.com>
2019-10-26 21:21:05 +0800

    1, 增加租户表、全局用户表，并将原来的数据库合并成zuihou_defaults、zuihou_base_{TENANT_CODE}库 2，完善不符合规则的自定义sql 3，增加J2Cache全局操作类

--- src/test/java/TestAuthorityGenerator.java
+++ src/test/java/TestAuthorityGenerator.java
@@ -22,21 +22,21 @@ public class TestAuthorityGenerator {
      * @param args
      */
     public static void main(String[] args) {
-        CodeGeneratorConfig build = buildAuthEntity();
+//        CodeGeneratorConfig build = buildDefaultsEntity();
 //        CodeGeneratorConfig build = buildAuthSuperEntity();
+        CodeGeneratorConfig build = buildAuthEntity();
 //        CodeGeneratorConfig build = buildCommonEntity();
 //        CodeGeneratorConfig build = buildCommonSuperEntity();
 //        CodeGeneratorConfig build = buildCoreEntity();
 
-        build.setUrl("jdbc:mysql://127.0.0.1:3306/zuihou_authority_new?serverTimezone=CTT&characterEncoding=utf8&useUnicode=true&useSSL=false&autoReconnect=true&zeroDateTimeBehavior=convertToNull");
         build.setUsername("root");
         build.setPassword("root");
         System.out.println("输出路径：");
         System.out.println(System.getProperty("user.dir") + "/zuihou-backend/zuihou-authority");
         build.setProjectRootPath(System.getProperty("user.dir") + "/zuihou-backend/zuihou-authority");
 
-//        FileCreateConfig fileCreateConfig = new FileCreateConfig(null);
-        FileCreateConfig fileCreateConfig = new FileCreateConfig(GenerateType.OVERRIDE);
+        FileCreateConfig fileCreateConfig = new FileCreateConfig(null);
+//        FileCreateConfig fileCreateConfig = new FileCreateConfig(GenerateType.OVERRIDE);
         fileCreateConfig.setGenerateEntity(GenerateType.OVERRIDE);
         fileCreateConfig.setGenerateEnum(GenerateType.OVERRIDE);
         fileCreateConfig.setGenerateDto(GenerateType.IGNORE);
@@ -61,19 +61,33 @@ public class TestAuthorityGenerator {
         CodeGenerator.main(build);
     }
 
+    public static CodeGeneratorConfig buildDefaultsEntity() {
+        List<String> tables = Arrays.asList(
+                "d_global_user",
+                "d_tenant"
+        );
+        CodeGeneratorConfig build = CodeGeneratorConfig.
+                build("authority", "", "zuihou", "d_", tables);
+        build.setSuperEntity(EntityType.ENTITY);
+        build.setChildPackageName("defaults");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/zuihou_defaults?serverTimezone=CTT&characterEncoding=utf8&useUnicode=true&useSSL=false&autoReconnect=true&zeroDateTimeBehavior=convertToNull");
+        return build;
+    }
+
     public static CodeGeneratorConfig buildAuthEntity() {
         List<String> tables = Arrays.asList(
-                "c_auth_application"
-                , "c_auth_menu"
-                , "c_auth_micro_service"
-                , "c_auth_resource"
-                , "c_auth_role"
-                , "c_auth_user"
+//                "c_auth_application"
+//                , "c_auth_menu"
+//                , "c_auth_micro_service"
+//                , "c_auth_resource"
+//                , "c_auth_role"
+                "c_auth_user"
         );
         CodeGeneratorConfig build = CodeGeneratorConfig.
                 build("authority", "", "zuihou", "c_auth_", tables);
         build.setSuperEntity(EntityType.ENTITY);
         build.setChildPackageName("auth");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/zuihou_base_0000?serverTimezone=CTT&characterEncoding=utf8&useUnicode=true&useSSL=false&autoReconnect=true&zeroDateTimeBehavior=convertToNull");
         return build;
     }
 
@@ -87,8 +101,10 @@ public class TestAuthorityGenerator {
                 build("authority", "", "zuihou", "c_auth_", tables);
         build.setSuperEntity(EntityType.SUPER_ENTITY);
         build.setChildPackageName("auth");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/zuihou_base_0000?serverTimezone=CTT&characterEncoding=utf8&useUnicode=true&useSSL=false&autoReconnect=true&zeroDateTimeBehavior=convertToNull");
         return build;
     }
+
     public static CodeGeneratorConfig buildCommonEntity() {
         List<String> tables = Arrays.asList(
                 "c_common_area"
@@ -99,8 +115,10 @@ public class TestAuthorityGenerator {
                 build("authority", "", "zuihou", "c_common_", tables);
         build.setSuperEntity(EntityType.ENTITY);
         build.setChildPackageName("common");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/zuihou_base_0000?serverTimezone=CTT&characterEncoding=utf8&useUnicode=true&useSSL=false&autoReconnect=true&zeroDateTimeBehavior=convertToNull");
         return build;
     }
+
     public static CodeGeneratorConfig buildCommonSuperEntity() {
         List<String> tables = Arrays.asList(
                 "c_common_opt_log"
@@ -110,8 +128,10 @@ public class TestAuthorityGenerator {
                 build("authority", "", "zuihou", "c_common_", tables);
         build.setSuperEntity(EntityType.SUPER_ENTITY);
         build.setChildPackageName("common");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/zuihou_base_0000?serverTimezone=CTT&characterEncoding=utf8&useUnicode=true&useSSL=false&autoReconnect=true&zeroDateTimeBehavior=convertToNull");
         return build;
     }
+
     public static CodeGeneratorConfig buildCoreEntity() {
         List<String> tables = Arrays.asList(
                 "c_core_org"
@@ -121,6 +141,7 @@ public class TestAuthorityGenerator {
                 build("authority", "", "zuihou", "c_core_", tables);
         build.setSuperEntity(EntityType.ENTITY);
         build.setChildPackageName("core");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/zuihou_base_0000?serverTimezone=CTT&characterEncoding=utf8&useUnicode=true&useSSL=false&autoReconnect=true&zeroDateTimeBehavior=convertToNull");
         return build;
     }
 }
